364533c9c397c689038f653a1a5e53ae90dbf1c7
iyy <chengzhangiyy@163.com#macBookPro>
2018-04-25 16:59:11 +0800

    wechat手机号node解密测试

--- src/model/index.js
+++ src/model/index.js
@@ -1,74 +1,36 @@
 const request = require("../utils/request"); //封装响应拦截器
 const orgRequest = require("request");
+var WXBizDataCrypt = require("../decode/WXBizDataCrypt");
+
+const APPID = 'wxbfe2c6898c6edd6a'//小程序后台复制
 
 module.exports = {
-  //服务基础地址
-  get baseUrl() {
-    return "http://tingapi.ting.baidu.com/v1/restserver/ting";
-  },
+    //服务基础地址
+    get baseUrl() {
+        return "https://api.weixin.qq.com";
+    },
 
-  //搜索
-  get search() {
-    return async function(keyWords) {
-      let name = decodeURI(keyWords),
-        url =
-          "?format=json&version=2&from=0&method=baidu.ting.search.catalogSug&query=" +
-          encodeURI(name),
-        data = await request.get(this.baseUrl + "?" + url);
-      return data;
-    };
-  },
+    //获取session_key
+    get sessionKey() {
+        return async function({code = ''}) {
+            let SECRET = '83f2dc9cbb0ed5410cc961cfb10fc0b9',//小程序后台复制
+                JSCODE = code//前端登录获取
+            data = await request.get(this.baseUrl + `/sns/jscode2session?appid=${APPID}&secret=${SECRET}&js_code=${JSCODE}&grant_type=authorization_code`);
+            //data 中有session_key
+            return data;
+        };
+    },
+    get phoneNumber(){
+      //解密换取信息
+        return async function ({sessionKey = '',iv = '',encryptedData = ''}) {
 
-  //歌曲详情
-  get songDetail() {
-    return async function(id) {
-      let url = this.baseUrl + "?method=baidu.ting.song.play&songid=" + id,
-        data = await request.get(url);
-      return data;
-    };
-  },
+            var pc = new WXBizDataCrypt(APPID, sessionKey);
 
-  //歌曲流
-  get mediaStream() {
-    return function(fileLink) {
-      let options = {
-        url: fileLink,
-        headers: {
-          "Content-Type": "application/octet-stream"
+            var data = pc.decryptData(encryptedData, iv);
+
+            console.log("解密后 data: ", data);
+            return data
         }
-      };
-      return orgRequest(options);
-    };
-  },
+    }
 
-  //艺人信息
-  get artistInfo() {
-    return async function(uid) {
-      let url =
-          this.baseUrl + "?method=baidu.ting.artist.getInfo&tinguid=" + uid,
-        data = await request.get(url);
-      return data;
-    };
-  },
-
-  //歌词
-  get lry() {
-    return async function(id) {
-      let url = this.baseUrl + "?method=baidu.ting.song.lry&songid=" + id,
-        data = await request.get(url);
-      return data;
-    };
-  },
-  get getAll() {
-    return async function(baseUrl = this.baseUrl, query) {
-      console.log("baseUrl:" + (baseUrl || this.baseUrl));
-      console.log("query:" + query);
-      console.log(
-        "fullUrl:" + encodeURI(`${baseUrl || this.baseUrl}?${query}`)
-      );
-      let fullUrl = encodeURI(baseUrl || this.baseUrl + "?" + query),
-        data = await request.get(fullUrl);
-      return data;
-    };
-  }
 };

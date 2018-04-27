f7cb1c98a7ee46e6aba055275fc2fc6aafcd6bc3
iyy <chengzhangiyy@163.com#macBookPro>
2018-04-27 17:32:12 +0800

    注释两行

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,10 +3,12 @@ const apiProxy = require("../model/index");
 
 const router = new Router();
 
-//搜索
+//登录
 router
     .post("/login", async ctx => {
+        //前端传过来的参数 ctx.request.body
         let data = await apiProxy.login(ctx.request.body);
+        //获取请求后端返回的数据，传递给前端
         ctx.body = data;
     })
 

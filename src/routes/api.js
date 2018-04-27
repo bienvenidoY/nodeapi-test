f307cce24a123d8876e6f1f74a3fbbfa62cd80fc
iyy <chengzhangiyy@163.com#macbookpro>
2018-04-27 14:02:45 +0800

    删除解析post参数代码
    (cherry picked from commit 44cea55)

--- src/routes/api.js
+++ src/routes/api.js
@@ -5,9 +5,9 @@ const router = new Router();
 
 //搜索
 router
-  .post("/session", async ctx => {
-    let data = await apiProxy.login();
-    ctx.body = data;
-  })
+    .post("/login", async ctx => {
+        let data = await apiProxy.login(ctx.request.body);
+        ctx.body = data;
+    })
 
 module.exports = router;
